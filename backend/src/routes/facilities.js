385c9518490b1be454d7e409e2076bc0ab0a56b1
KyLeo25 <vertusarmageddon88@gmail.com>
2026-07-03 23:33:11 +0700

    Swagger facilites.js

--- backend/src/routes/facilities.js
+++ backend/src/routes/facilities.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/facilityController');
 
+/**
+ * @swagger
+ * /facilities:
+ *   get:
+ *     summary: Ambil daftar fasilitas umum kota (opsional filter berdasarkan jenis)
+ *     tags: [Facilities]
+ *     security: []
+ *     parameters:
+ *       - in: query
+ *         name: jenis
+ *         required: false
+ *         schema: { type: string }
+ *         example: "Taman"
+ *         description: Filter fasilitas berdasarkan jenis
+ *     responses:
+ *       200:
+ *         description: Daftar fasilitas berhasil diambil
+ */
 router.get('/', ctrl.getAll);
 
 module.exports = router;
